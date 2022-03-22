ffa3a295fb01e4b53ac5d1c238e4817519d2e1e5
VitaliyLipovetskiy <vitaliy1972li@gmail.com>
2022-03-22 20:22:57 +0200

    first

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestaurantVotingApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(RestaurantVotingApplication.class, args);
-
     }
 }
